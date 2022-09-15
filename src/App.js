3a3aa5e05d5e29961b95e63c6fd4a1da5b7790a5
Mike Hoyt <mhoyt9363@gmail.com>
2022-09-15 16:02:45 -0400

    updating for second attempt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello CSCI E-39!</p>
-        <p>
-          My name is Mike Hoyt
-        </p>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello CSCI E-39!</p>
+          <p>
+            My name is Mike Hoyt
+          </p>
+        </header>
+      </div>
+    );
+  }
 
-export default App;
+export default App; 
